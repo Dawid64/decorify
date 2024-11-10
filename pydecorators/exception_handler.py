084bf2f8b330e6f5f562ee1b3f25172cb603ead0
Dawid64 <dawid.siera@gmail.com>
2024-11-11 00:35:22 +0100

    Added documentation for typehint validator

--- pydecorators/exception_handler.py
+++ pydecorators/exception_handler.py
@@ -1,3 +1,4 @@
+""" Python module for exception handling and validation """
 from typing import Callable, Optional
 from .base import decorator
 from functools import wraps
@@ -23,6 +24,18 @@ def exception_handling_default_value(func: Callable, default_value=None, *, logg
 
 @decorator
 def validate_typehints(func: Callable):
+    """ Checks if arguments passed to wrapped functions, are instances of typehint classes
+    If not raises Value error.
+    Decorator does not change the return value, it's recommended if it's important to check correctness of given types.
+
+    **!! Be aware that this may increase the function runtime. Not recommended for very simple functions. !!**
+
+    Raises
+    -------
+    Value Error:
+        Raises value error if passed arguments does not match the typehints.
+
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         all_arguments = dict(zip(inspect.getfullargspec(func).args, args))

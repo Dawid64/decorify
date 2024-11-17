3b6ddc5abc5b8e6f6ab45283ff7e5aad84521bbb
Dawid64 <dawid.siera@gmail.com>
2024-11-17 17:30:11 +0100

    Added _DecoratorMetaClass

--- decorify/base.py
+++ decorify/base.py
@@ -1,5 +1,5 @@
 """ Base Module with decorator structurer """
-from typing import Callable
+from typing import Any, Callable
 
 
 def decorator(dec: Callable):
@@ -11,3 +11,25 @@ def decorator(dec: Callable):
             return dec(*args, **kwargs, __func__=func)
         return wrapped_func
     return inner_func
+
+
+class _DecoratorMetaClass(type):
+    """
+    Metaclass to enable creation of class-based decorators.
+
+    This metaclass allows the class to be used as decorator with or without arguments.
+
+    Using decorator with arguments:
+        - the __init__ method is called with passed arguments.
+        - the __call__ method is called with the function to be decorated.
+
+    Using decorator without arguments:
+        - the __init__ method is called without any arguments (arguments takes default values).
+        - the __call__ method is called with the function to be decorated.
+    """
+    def __call__(cls, *args, **kwargs):
+        if args and isinstance(args[0], Callable) and len(args) == 1 and not kwargs:
+            instance = cls.__new__(cls)
+            instance.__init__()
+            return instance.__call__(*args, **kwargs)
+        return super().__call__(*args, **kwargs)

36d4e21ff5b80dbcc94bf5d240b314ed7d917ec9
adam <adamtms@hotmail.com>
2024-11-20 18:43:07 +0100

    Add tests for redirect stderr

--- tests/test_io_redirect.py
+++ tests/test_io_redirect.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tempfile
 from uuid import uuid4
 from decorify import no_print, redirect_stdout, redirect_stderr
@@ -41,3 +42,36 @@ def test_redirect_stdout_none(capsys):
     
     print_hello()
     assert capsys.readouterr().out == ""
+
+def test_redirect_stderr_file(capsys):
+    with tempfile.TemporaryFile("w+") as f:
+        @redirect_stderr(f)
+        def print_hello():
+            print("Hello, world!", file=sys.stderr)
+        
+        print_hello()
+        f.seek(0)
+        assert f.read() == "Hello, world!\n"
+        assert capsys.readouterr().err == ""
+
+def test_redirect_stderr_str(capsys):
+    filename = f"temp_{uuid4()}.txt"
+    @redirect_stderr(filename)
+    def print_hello():
+        print("Hello, world!", file=sys.stderr)
+    
+    print_hello()
+    try:
+        with open(filename, "r") as f:
+            assert f.read() == "Hello, world!\n"
+        assert capsys.readouterr().err == ""
+    finally:
+        os.remove(filename)
+
+def test_redirect_stderr_none(capsys):
+    @redirect_stderr()
+    def print_hello():
+        print("Hello, world!", file=sys.stderr)
+    
+    print_hello()
+    assert capsys.readouterr().err == ""

bbaf3d1517525ba99363238d4e5e6ca167032c49
Dawid64 <dawid.siera@gmail.com>
2024-11-21 16:33:50 +0100

    Added tests for crawler and ascii generator

---
+++ tests/test_profiling.py
@@ -0,0 +1,42 @@
+import sys
+from decorify.profiling import crawler, Tree, generate_ascit_tree
+
+
+def test_profiling_stack():
+    def func_a():
+        return sum([1, 1])
+
+    @crawler(as_list=True)
+    def func_b():
+        return func_a() + func_a()
+
+    assert func_b() == ['func_b', ['func_a'], ['func_a']]
+
+
+def test_profiling_stack_build_ins():
+    def func_a():
+        return sum([1, 1])
+
+    @crawler(build_ins=True, as_list=True)
+    def func_b():
+        return func_a() + func_a()
+
+    assert func_b() == ['func_b', ['func_a', ['sum']], ['func_a', ['sum']]]
+
+
+def test_profiling_tree():
+    def func_a():
+        return sum([1, 1])
+
+    @crawler(as_tree=True)
+    def func_b():
+        return func_a() + func_a()
+
+    assert isinstance(func_b(), Tree)
+
+
+def test_ascii_generator():
+    sample = ['add_1', ['add_2', ['add_2', ['add_2', ['add_2', ['add_2'], ['add_2']], ['add_2']], [
+        'add_2', ['add_2'], ['add_2']]], ['add_2', ['add_2', ['add_2'], ['add_2']], ['add_2']]]]
+    ascii_tree = generate_ascit_tree(sample)
+    assert isinstance(ascii_tree, str)

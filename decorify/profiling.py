d12c0d91e84c9048771d8055dfda60ac138a64ba
Dawid64 <dawid.siera@gmail.com>
2024-11-21 16:32:05 +0100

    Added simple generator for ascii tree

--- decorify/profiling.py
+++ decorify/profiling.py
@@ -5,6 +5,33 @@ from typing import List, Optional
 from decorify.base import decorator
 
 
+def generate_ascit_tree(item, prefix='', is_last=False, is_root=True):
+    """Recursively generates ASCII tree from nested list."""
+    # TODO: can be changed into iterative approach
+    text = ''
+    if isinstance(item, list):
+        node = item[0]
+        children = item[1:]
+    else:
+        node = item
+        children = []
+
+    if is_root:
+        text += node + '\n'
+        child_prefix = ''
+    else:
+        connector = '└── ' if is_last else '├── '
+        text += prefix + connector + node + '\n'
+        child_prefix = prefix + ('    ' if is_last else '│   ')
+
+    last_child_index = len(children) - 1
+    for idx, child in enumerate(children):
+        is_child_last = idx == last_child_index
+        text += generate_ascit_tree(child, child_prefix,
+                                    is_child_last, is_root=False)
+    return text
+
+
 class Tree:
     """
     Tree-based structure
@@ -17,12 +44,15 @@ class Tree:
 
     def to_list(self):
         if not self.childrens:
-            return self.name
-        return [self.name, [child.to_list() for child in self.childrens]]
+            return [self.name]
+        return [self.name, *[child.to_list() for child in self.childrens]]
+
+    def __repr__(self):
+        return generate_ascit_tree(self.to_list())
 
 
 @decorator
-def crawler(build_ins: bool = False, as_list: bool = False, __func__=None):
+def crawler(build_ins: bool = False, as_list: bool = False, as_tree: bool = False, __func__=None):
     trace_tree: Tree = Tree('Base')
     head: Tree = trace_tree
 
@@ -48,7 +78,12 @@ def crawler(build_ins: bool = False, as_list: bool = False, __func__=None):
         sys.setprofile(profiler)
         res = __func__(*args, **kwargs)
         sys.setprofile(current_profiler)
+        nonlocal trace_tree, head
         func_trace = trace_tree.childrens[0]
+        trace_tree = Tree('Base')
+        head = trace_tree
+        if as_tree:
+            return func_trace
         if as_list:
             return func_trace.to_list()
         return res

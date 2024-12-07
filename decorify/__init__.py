d56c25a9acc6a34ffa4db589e256281dca2367d8
Dawid64 <dawid.siera@gmail.com>
2024-12-07 19:03:17 +0100

    Added profiler to init

--- decorify/__init__.py
+++ decorify/__init__.py
@@ -8,7 +8,7 @@ Decorify  is a lightweight Python library without any dependencies that offers a
 from decorify.base import decorator
 from decorify.basic import timeit, grid_search, timeout
 from decorify.exceptions import default_value, validate_typehints
-from decorify.profiling import crawler
+from decorify.profiling import crawler, profiler
 
 __all__ = ['timeit', 'grid_search', 'timeout',
-           'default_value', 'validate_typehints', 'crawler']
+           'default_value', 'validate_typehints', 'crawler', 'profiler']

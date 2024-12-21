0b7331a5d1a80cae122c6530279065fdbd8457d3
TCA <tomek.chady@gmail.com>
2024-12-21 14:15:27 +0100

    Added two decorators for profiling memory

--- tests/test_profiling.py
+++ tests/test_profiling.py
@@ -1,4 +1,5 @@
-from decorify.profiling import crawler, Tree, generate_ascii_tree
+from decorify.profiling import crawler, Tree, generate_ascii_tree, track_leaks, measure_memory_usage
+import sys
 
 
 def test_profiling_stack():
@@ -39,3 +40,36 @@ def test_ascii_generator():
         'add_2', ['add_2'], ['add_2']]], ['add_2', ['add_2', ['add_2'], ['add_2']], ['add_2']]]]
     ascii_tree = generate_ascii_tree(sample)
     assert isinstance(ascii_tree, str)
+
+def test_track_leaks():
+    @track_leaks
+    def func_a():
+        return sum([1, 1])
+    
+    _, leaked = func_a()
+    assert leaked == 0
+    
+    # I have heard this is a memory leak, but I am not sure if it actually is
+    # this decorator sure thinks it is
+    # MAYBE create our own bad C mini extension with a memory leak and test it.
+    @track_leaks
+    def func_leak():
+        try:
+            raise Exception
+        except Exception as e:
+            etype, evalue, tb = sys.exc_info()
+            return tb
+        
+    _, leaked = func_leak()
+    assert leaked == 3
+    
+def test_track_memory():
+    
+    @measure_memory_usage
+    def func_a():
+        return 1
+    
+    ret, memory = func_a()
+    ret_size = sys.getsizeof(ret)
+    assert memory >= ret_size
+    

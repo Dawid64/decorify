95dc060e939ba087fd66c55aec3a4bdd3592f200
Dawid64 <dawid.siera@gmail.com>
2025-03-14 21:57:59 +0100

    Minor changes, error preferred to warning

--- decorify/configuration.py
+++ decorify/configuration.py
@@ -5,20 +5,21 @@ from inspect import signature
 from logging import Logger
 from typing import Optional, Callable
 from pathlib import Path
-from sys import version_info 
+from sys import version_info
 from json import load as json_load
 from .base import decorator
 from .exceptions import validate_typehints
 
+
 @decorator
-def configure(path: str|dict|None = None, *, logger: Optional[Logger] = None, __func__: Callable = None) -> Callable:
+def configure(path: str | dict | None = None, *, logger: Optional[Logger] = None, __func__: Callable = None) -> Callable:
     """
     Decorator for configuring function values from a file or a dictionary.
 
     This decorator reads configuration data from the provided source and applies
     it to the decorated function's arguments.
     The order of poisitonal arguments is preserved.
-    
+
     Files supported: .yaml, .yml, .txt, .toml, .json
 
     ## Parameters
@@ -28,7 +29,7 @@ def configure(path: str|dict|None = None, *, logger: Optional[Logger] = None, __
     ## Returns
     function - Wrapped function with configured values
 
-    
+
     ### Configuration
     - If an positional argument wasn't defined in the function call it will be passed as keyword argument from configuration if defined,
     otherwise a ValueError will be raised.
@@ -43,18 +44,19 @@ def configure(path: str|dict|None = None, *, logger: Optional[Logger] = None, __
     """
     @wraps(__func__)
     def wrapper(*args, **kwargs):
-        config = path if isinstance(path,dict) else _open_file(path, logger)
-        if not isinstance(config,dict):
-            if isinstance(logger,Logger):
+        config = path if isinstance(path, dict) else _open_file(path, logger)
+        if not isinstance(config, dict):
+            if isinstance(logger, Logger):
                 logger.warning("Configuration file not formatted properly, can't decode the dict returning the funciton without configuration")
-            return __func__(*args, **kwargs)   
+            return __func__(*args, **kwargs)
 
-        for name, info in list(signature(__func__).parameters.items())[len(args):]:
-            if name not in kwargs and name in config: 
+        for name, _ in list(signature(__func__).parameters.items())[len(args):]:
+            if name not in kwargs and name in config:
                 kwargs[name] = config[name]
 
         return validate_typehints(__func__)(*args, **kwargs)
     return wrapper
+
 
 def _open_file(path, logger: Optional[Logger] = None):
     """
@@ -68,36 +70,24 @@ def _open_file(path, logger: Optional[Logger] = None):
 
     match Path(path).suffix:
         case '.yaml' | '.yml':
-            try:
-                import yaml
-            except:
-                if isinstance(logger,Logger):
-                    logger.warning("Yaml packackege is not installed, you can instal it using 'pip install pyyaml', returning the function without configuration")
-                return {}
+            import yaml
             with open(path, 'r') as f:
                 return yaml.load(f, Loader=yaml.FullLoader)
 
         case '.txt':
             with open(path, 'r') as f:
                 return eval(f.read())
-            
+
         case '.toml':
-            if version_info >=  (3, 11):  # Python 3.11+
+            if version_info >= (3, 11):  # Python 3.11+
                 import tomllib as toml
             else:
-                try:
-                    import toml
-                except:
-                    if isinstance(logger, Logger):
-                        logger.warning("Toml packackege is not installed, you can instal it using 'pip install toml' or upgrade to python 3.11+ to have it in standard libraries, returning the function without configuration")
-                    return {}
+                import toml
             with open(path, 'rb') as f:
                 return toml.load(f)
-            
+
         case '.json':
             with open(path, 'r') as f:
                 return json_load(f)
-        case _: 
-            if isinstance(logger, Logger):
-                logger.warning("Unsuported config file type, returning the function without configuration")
-            return {}
+        case _:
+            raise ValueError(f'Format: {Path(path).suffix} is not currently supported')
